c26323ba5024ffed68783d61884a3af70a2c1d77
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-11-24 21:53:08 +0330

    Minor (IMPORTANT: relying more on "facro.sty")
    - TeX snippets are dependent on facro.sty (on `shared-docs/texmf`). Will
    be copied over soon enough.
    - Add pass package.
    - Remove some of default packages and moved them to facro.
    - Add a few global LaTeX macros.

--- plugins/+xetex.el
+++ plugins/+xetex.el
@@ -19,13 +19,13 @@
         (setq
          org-latex-compiler "xelatex"
          org-latex-pdf-process (mapcar (lambda (s) (replace-regexp-in-string "%latex" "xelatex" s)) org-latex-pdf-process)) ; ensures proper compilation
-        (add-to-list 'org-latex-packages-alist '("pass" "geometry")) ; this prevents buggy page resizes in XeTeX
-        (dolist (pkg '("fontspec"
-                       "xpatch"
-                       "letltxmacro"
-                       "fullpage"
-                       "caption"
-                       "float"
-                       "xcolor"
-                       "titling"))
-                (add-to-list 'org-latex-packages-alist  (list "" pkg))))
+        ;; (add-to-list 'org-latex-packages-alist '("pass" "geometry")) ; this prevents buggy page resizes in XeTeX ; prevents compilation in beamer
+        (add-to-list 'org-latex-packages-alist '("" "fontspec"))
+        (add-to-list 'org-latex-packages-alist '("" "caption"))
+        (add-to-list 'org-latex-packages-alist '("" "float"))
+        (add-to-list 'org-latex-packages-alist '("" "xcolor"))
+        (add-to-list 'org-latex-packages-alist '("" "tikz"))
+        (add-to-list 'org-export-global-macros '(lr  . "@@latex:\\lr{@@​$1​@@latex:}@@"))
+        (add-to-list 'org-export-global-macros '(fn  . "[fn::$1]"))
+        (add-to-list 'org-export-global-macros '(lfn . "[fn::@@latex:\\lr{@@​$1​@@latex:}@@]"))
+        (add-to-list 'org-export-global-macros '(thislink . "(eval (concat \"[[file:\" (file-name-sans-extension (buffer-name)) $1 \"]]\"))")))
